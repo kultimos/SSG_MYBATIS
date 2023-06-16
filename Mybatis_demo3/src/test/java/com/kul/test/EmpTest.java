30c24cf62bd9aefdb181021e0b8e9c11ab540968
wushuai <659-4gpe145ln@dingtalk.com>
2023-06-16 16:27:06 +0800

    p61:mybatis整合第三方缓存EHCache- -

--- Mybatis_demo3/src/test/java/com/kul/test/EmpTest.java
+++ Mybatis_demo3/src/test/java/com/kul/test/EmpTest.java
@@ -30,21 +30,21 @@ public class EmpTest {
 
     private SqlSession sqlSession;
 
-    @Before
-    public void init() throws IOException {
-        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
-        //获取sqlSessionFactoryBuilder对象
-        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
-        //获取sqlSessionFactory对象
-        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
-        //获取sql的会话对象SqlSession,MyBatis提供的操作数据库的对象,参数true表示自动提交事务
-        sqlSession = sqlSessionFactory.openSession(true);
-        //获取UserMapper的代理实现类对象
-        empMapper = sqlSession.getMapper(EmpMapper.class);
-        deptMapper = sqlSession.getMapper(DeptMapper.class);
-        dynamicSQLMapper = sqlSession.getMapper(DynamicSQLMapper.class);
-        cacheMapper = sqlSession.getMapper(CacheMapper.class);
-    }
+//    @Before
+//    public void init() throws IOException {
+//        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+//        //获取sqlSessionFactoryBuilder对象
+//        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+//        //获取sqlSessionFactory对象
+//        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+//        //获取sql的会话对象SqlSession,MyBatis提供的操作数据库的对象,参数true表示自动提交事务
+//        sqlSession = sqlSessionFactory.openSession(true);
+//        //获取UserMapper的代理实现类对象
+//        empMapper = sqlSession.getMapper(EmpMapper.class);
+//        deptMapper = sqlSession.getMapper(DeptMapper.class);
+//        dynamicSQLMapper = sqlSession.getMapper(DynamicSQLMapper.class);
+//        cacheMapper = sqlSession.getMapper(CacheMapper.class);
+//    }
 
     @Test
     public void test() {
